09f67a436e6e2c2df9a92d96c050c5f00cda6d41
kashitani0530 <ksy05x30inter@yahoo.co.jp>
2018-06-14 17:54:34 +0900

    6/14

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//Hello World! の出力
+		String abc ="山田";
+		System.out.println(abc + "さん");
+	}
+
+}
